ce158f8d001c149798b45dc0b93ce57bd45ab6b7
Maxim <maxim.nalimov@o.fair.com>
2022-05-15 20:20:25 +0500

    playing with keyboard

--- src/index.py
+++ src/index.py
@@ -1,37 +1,26 @@
-# import json
+import json
 
 
 def handler(event, context):
-    # body = json.loads(event['body'])
+    body = json.loads(event['body'])
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json'
         },
-        # 'body': json.dumps({
-        #     'method': 'sendMessage',
-        #     'chat_id': body['message']['chat']['id'],
-        #     'text':  {
-        #         "keyboard": [["uno :+1:"],
-        #                      ["uno \ud83d\udc4d", "due"],
-        #                      ["uno", "due", "tre"],
-        #                      ["uno", "due", "tre", "quattro"]]
-        #     },
-        #     "reply_markup": {"ReplyKeyboardMarkup": {
-        #         "keyboard": [
-        #             [{"KeyboardButton": {"text": "test1"}}]]}},
-        # }),
-        "reply_markup": {
-            "inline_keyboard": [[
-                {
-                    "text": "A",
-                    "callback_data": "A1"
-                },
-                {
-                    "text": "B",
-                    "callback_data": "C1"
-                }]
-            ]
-        },
+        'body': json.dumps({
+            'method': 'ReplyKeyboardMarkup',
+            'chat_id': body['message']['chat']['id'],
+            # 'text':  {
+            #     "keyboard": [["uno :+1:"],
+            #                  ["uno \ud83d\udc4d", "due"],
+            #                  ["uno", "due", "tre"],
+            #                  ["uno", "due", "tre", "quattro"]]
+            # },
+            "keyboard": [["uno :+1:"],
+                         ["uno \ud83d\udc4d", "due"],
+                         ["uno", "due", "tre"],
+                         ["uno", "due", "tre", "quattro"]]
+        }),
         'isBase64Encoded': False
     }
