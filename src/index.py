4d69d6ed7ca47e03737caec32e8e081db2493ad5
Maxim <maxim.nalimov@o.fair.com>
2022-05-15 20:46:38 +0500

    playing with keyboard

--- src/index.py
+++ src/index.py
@@ -11,7 +11,7 @@ def handler(event, context):
         'body': json.dumps({
             'method': 'InputMessageContent',
             'parse_mode': 'HTML',
-            'text':  '<html><body>Hello</body><footer>world</footer></html>',
+            'message_text':  '<html><body>Hello</body><footer>world</footer></html>',
         }),
         'isBase64Encoded': False
     }
