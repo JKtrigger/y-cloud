6832707615fcdeb31ebcff376883d838b9342550
Maxim <maxim.nalimov@shift.com>
2022-05-17 15:00:31 +0500

    playung with Keyboard

--- src/index.py
+++ src/index.py
@@ -1,19 +1,24 @@
 import json
-from telegram import TelegramObject
 
 
 def handler(event, context):
-    print(TelegramObject)
-    #body = json.loads(event['body'])
+    body = json.loads(event['body'])
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json'
         },
         'body': json.dumps({
-            'method': 'InputMessageContent',
-            'parse_mode': 'HTML',
-            'message_text':  '<html><body>Hello</body><footer>world</footer></html>',
-        }),
+            'method': 'sendMessage',
+            'chat_id': body['message']['chat']['id'],
+            'text': body,
+            'reply_markup': {
+                'ReplyKeyboardMarkup':
+                    {'keyboard': [
+                        ["uno :+1:"],
+                        ["uno \ud83d\udc4d", "due"],
+                        ["uno", "due", "tre"],
+                        ["uno", "due", "tre", "quattro"]]},
+            }),
         'isBase64Encoded': False
     }
