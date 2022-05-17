84da2d457995214551a77307a18dc26bf66864e7
Maxim <maxim.nalimov@shift.com>
2022-05-17 15:11:58 +0500

    playung with Keyboard

--- src/index.py
+++ src/index.py
@@ -9,16 +9,15 @@ def handler(event, context):
             'Content-Type': 'application/json'
         },
         'body': json.dumps({
-            'method': 'sendMessage',
+            'method': 'ReplyKeyboardMarkup',
             'chat_id': body['message']['chat']['id'],
             'text': body,
             'reply_markup': {
-                'ReplyKeyboardMarkup':
-                    {'keyboard': [
-                        ["uno :+1:"],
-                        ["uno \ud83d\udc4d", "due"],
-                        ["uno", "due", "tre"],
-                        ["uno", "due", "tre", "quattro"]]},
+                'keyboard': [
+                    ["uno :+1:"],
+                    ["uno \ud83d\udc4d", "due"],
+                    ["uno", "due", "tre"],
+                    ["uno", "due", "tre", "quattro"]],
             }
         }),
         'isBase64Encoded': False
