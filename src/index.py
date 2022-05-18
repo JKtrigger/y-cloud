5bbd03ec530373724d9dd3b87b21004642fa7158
Maxim <maxim.nalimov@shift.com>
2022-05-18 15:47:45 +0500

    ...

--- src/index.py
+++ src/index.py
@@ -1,6 +1,10 @@
+import json
+
 from .application import commands
 from .telegram import BotChat
 
 
 def handler(event, _context):
+    body = json.loads(event['body'])
+    print(body)
     return commands.execute(BotChat(event))
