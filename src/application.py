5833c13b67942f400790b3496f412e72720545e1
Maxim <maxim.nalimov@shift.com>
2022-05-19 19:22:26 +0500

    playing with Keyboard

--- src/application.py
+++ src/application.py
@@ -22,22 +22,22 @@ def start(request: BotChat):
     }
 
 
-def end(request: BotChat):
-    return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'isBase64Encoded': False,
-        'body': json.dumps({
-            'method': 'sendMessage',
-            'chat_id': request.chat_id,
-            'text': 'any text',
-            'reply_markup': {'inline_keyboard': [
-                [{"text": "/start", "callback_data": "1"}, {"text": "/start", "callback_data": "1"}],
-                [{"text": "/end", "callback_data": "2"}],
-            ], 'resize_keyboard': True},
-        })
-    }
+# def end(request: BotChat):
+#     return {
+#         'statusCode': 200,
+#         'headers': {'Content-Type': 'application/json'},
+#         'isBase64Encoded': False,
+#         'body': json.dumps({
+#             'method': 'sendMessage',
+#             'chat_id': request.chat_id,
+#             'text': 'any text',
+#             'reply_markup': {'inline_keyboard': [
+#                 [{"text": "/start", "callback_data": "1"}, {"text": "/start", "callback_data": "1"}],
+#                 [{"text": "/end", "callback_data": "2"}],
+#             ], 'resize_keyboard': True},
+#         })
+#     }
 
 
 commands.add_handler(start, '/start')
-commands.add_handler(end, '/end')
+# commands.add_handler(end, '/end')
