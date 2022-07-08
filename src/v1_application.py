826fcdd85b54d7ff4e90ad5b37e0ec752f39c795
Maxim <maxim.nalimov@shift.com>
2022-07-08 19:09:20 +0500

    add sendMediaGroup method

--- src/v1_application.py
+++ src/v1_application.py
@@ -1,5 +1,34 @@
+import boto3
+
 from src.telegram.v1 import Event, response, HTTP200
 from src.telegram.v1.botchat import Listener
+
+endpoint_url = 'https://storage.yandexcloud.net'
+prefix = 'photo/'
+bucket = 'booking'
+
+session_ = boto3.session.Session()
+s3 = session_.client(service_name='s3', endpoint_url=endpoint_url)
+
+texts = [  # TODO make possibility to change description
+    """
+    Атмосферное место ДОМ и БАНЯ!
+    Дом сдаётся минимум на двое суток!!
+    Цена указана при бронировании от 7 дней.
+    Предлагаем к бронированию июль и август! 
+    Свободные даты уточняйте!
+    """,
+    """
+    Деревянный,уютный,теплый дом с баней.
+    В доме имеется все необходимые для хорошего отдыха,гостинная,кухня,две спальни и два санузла.
+    Максимальное количество 6 человек.Дом расположен в жилом поселке,рядом 
+    автобусная остановка,до моря 10 мин ходьбы.
+    Территория ухоженная,на участке есть пруд с декоративными рыбками,в пруду купаться можно!"""
+    ,
+    """При заезде требуется залоговая сумма 10000р. На вcякий случай""",
+    """Подтверждение бронирования 10% от суммы брони. Цена зу сутки 7500р """,
+    """Внимание - отмена брони до 50% при условии отмены сделки"""
+]
 
 
 @response(HTTP200)
@@ -15,12 +44,26 @@ def main_menu(body: dict, chat_id):
         }
 
 
+def get_url_photos() -> list:
+    expression = f"{endpoint_url}/{bucket}/{{key}}".format
+    contents = s3.list_objects(Bucket=bucket, Prefix=prefix)['Contents']
+    return [expression(key=key['Key']) for key in contents if key['Size']]
+
+
+def get_photos_in_media_format():
+    mapping_attr: callable = lambda x: {'type': 'photo', 'media': x}
+    return [mapping_attr(i) for i in get_url_photos()]
+
+
 @response(HTTP200)
 def photo(body: dict, chat_id):
     return {
-            'method': 'sendMessage',
+            'method': 'sendMediaGroup',  # is can be sending as document ?  sendDocument
+                                         # sendPhoto sendMediaGroup,
+            'allow_sending_without_reply': False,
             'chat_id': chat_id,
-            'text': 'any text',
+            'media': get_photos_in_media_format(),
+            'protect_content': True,
             'reply_markup': {'keyboard': [
                 ['🏠 Дом', '☭ Участок'],
                 ['⚓ Море', 'Назад']
---
+++ test/v1/test_s3_bucket.py
@@ -0,0 +1,12 @@
+import pytest
+
+from src.v1_application import get_url_photos, get_photos_in_media_format
+
+
+@pytest.mark.skip("manual test")
+def test_bucket_urls():
+    assert get_url_photos(), 'Photos are included ? '
+
+
+def test_mapping_media():
+    assert get_photos_in_media_format(), 'Photos are included ? '
