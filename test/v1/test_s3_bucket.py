cb807b228e5393bfeb3d9473ddeb32dfee016e03
Maxim <maxim.nalimov@shift.com>
2022-07-08 21:01:04 +0500

    hardcoded images

--- test/v1/test_s3_bucket.py
+++ test/v1/test_s3_bucket.py
@@ -1,14 +1,14 @@
-import boto3
-import pytest
-
-from src.v1_application import get_url_photos, get_photos_in_media_format
-
-
-@pytest.mark.skip("manual test")
-def test_bucket_urls():
-    assert get_url_photos(), 'Photos are included ? '
-
-
-@pytest.mark.skip("manual test")
-def test_mapping_media():
-    assert get_photos_in_media_format(), 'Photos are included ? '
+# import boto3
+# import pytest
+#
+# from src.v1_application import get_url_photos, get_photos_in_media_format
+#
+#
+# @pytest.mark.skip("manual test")
+# def test_bucket_urls():
+#     assert get_url_photos(), 'Photos are included ? '
+#
+#
+# @pytest.mark.skip("manual test")
+# def test_mapping_media():
+#     assert get_photos_in_media_format(), 'Photos are included ? '
