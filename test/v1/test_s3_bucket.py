81dbdb0711ade3db5ff32a7c10d172353f7998d8
Maxim <maxim.nalimov@shift.com>
2022-07-08 19:11:49 +0500

    add sendMediaGroup method

--- test/v1/test_s3_bucket.py
+++ test/v1/test_s3_bucket.py
@@ -8,5 +8,6 @@ def test_bucket_urls():
     assert get_url_photos(), 'Photos are included ? '
 
 
+@pytest.mark.skip("manual test")
 def test_mapping_media():
     assert get_photos_in_media_format(), 'Photos are included ? '
